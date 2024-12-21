d8bf9f0bd7295142b8a93aef00d2bc6af1b56580
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-12-21 11:23:12 +0100

    Create bump_reminder.py

---
+++ extensions/listener/bump_reminder.py
@@ -0,0 +1,24 @@
+import interactions
+from interactions.api.events import MessageCreate
+from src.logutil import init_logger
+import os
+import asyncio
+
+
+logger = init_logger(os.path.basename(__file__))
+
+disboard_id = 302050872383242240
+
+class BumpReminder(interactions.Extension):
+    @interactions.listen(MessageCreate)
+    async def on_message(self, event: MessageCreate):
+
+        if event.message.author.id != disboard_id:
+            return
+
+        logger.info("Bump received!")
+        await event.message.add_reaction("❤️")
+        # sleep for 2 hours
+        await asyncio.sleep(60*60*2)
+
+        await event.message.channel.send(e=interactions.Embed("Bump Reminder", "It's time to bump the server! :tada:"))
