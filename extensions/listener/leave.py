7410d48157982a828e1b2b8ed1db778c7f245094
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-12-21 09:50:53 +0100

    bug fix

--- extensions/listener/leave.py
+++ extensions/listener/leave.py
@@ -2,38 +2,22 @@ import os
 
 import interactions
 from interactions import listen
-from interactions.api.events import MemberAdd
+from interactions.api.events import MemberRemove
 from src.config import CONFIG
 from src import logutil
 
 
 logger = logutil.init_logger(os.path.basename(__file__))
 
-class Welcome(interactions.Extension):
-    """Welcome the user to the server"""
+class Leave(interactions.Extension):
+    """Listen for member leave events."""
 
-    @listen()
-    async def on_member_add(self, event: MemberAdd):
+    @listen(MemberRemove)
+    async def on_member_remove(self, event: MemberRemove):
         # logger.info(f"New member joined: {event.member.display_name}")
 
-        embed = interactions.Embed(
-            title=f"Welcome, {event.member.display_name}!",
-            description=f"Welcome on ArtsCafe! Have fun and enjoy your stay!",
-            color=interactions.Color.from_hex("ba6e5e")
-        )
-
-        embed.set_thumbnail(url=event.member.avatar.url)
-
-        role = event.guild.get_role(CONFIG.roles.member)
         welcome_channel = event.guild.get_channel(CONFIG.channels.welcome)
         if not isinstance(welcome_channel, interactions.GuildText):
             return
 
-        await welcome_channel.send(embeds=embed)
-        # logger.info(f"Sent welcome message for {event.member.display_name}")
-
-        if not role:
-            return
-
-        await event.member.add_role(role)
-        # logger.info(f"Added role {role.name} to {event.member.display_name}")
+        await welcome_channel.send(f"Sadly, {event.member.display_name} has left us. :wave:")
