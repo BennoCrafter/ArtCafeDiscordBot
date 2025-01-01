1d510a3a31f6aaf969b00c29d3d8a3ed5772bb39
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-01-01 20:27:41 +0100

    Update image_gallery.py

--- extensions/listener/image_gallery.py
+++ extensions/listener/image_gallery.py
@@ -3,6 +3,7 @@ from interactions.api.events import MessageCreate
 from src.logutil import init_logger
 import os
 import asyncio
+from src.config import CONFIG
 
 
 logger = init_logger(os.path.basename(__file__))
@@ -11,6 +12,9 @@ class ImageGallery(interactions.Extension):
     @interactions.listen(MessageCreate)
     async def on_message(self, event: MessageCreate):
         if len(event.message.attachments) == 0:
+            return
+        
+        if event.channel.id != CONFIG.channel.image_gallery:
             return
 
         await event.message.add_reaction("❤️")
